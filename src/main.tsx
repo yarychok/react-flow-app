1b6eb0bc9079dc014c07b516090362752d4858b4
Liubomyr Yarych <78175599+yarychok@users.noreply.github.com>
2023-06-23 19:57:37 +0300

    A few fixes

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import store, { persistor } from './store/store';
 import App from "./App"
 import "./App.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ReactFlowProvider>
       <Provider store={store}>
